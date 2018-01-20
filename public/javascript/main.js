7275b2b5fcee1976211dbfe676d9d2765a03e7f3
Steven Zafrani <zsteven44@gmail.com>
2018-01-20 15:21:49 -0800

    minor cleanup

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,19 +1,18 @@
-$(document).ready(function () {
-    $(window).bind('resize', set_img_height);
-    $('.home-header').imagefit();
-});
-
-function set_img_height () {
-
-    var portHeight = $(window).height();
-    $('#content').imagefit();
 
 
-/*
 
-    var portWidth = $(window).width();
-    $(".home-header").height(portHeight);
-    console.log("The viewport height is " + portHeight + " pixels and the viewport width is " + portWidth + " pixels.");
-    $(".home-header").css("height", portHeight).css("width",portWidth).css("background-repeat","no repeat");
-*/
+function start() {
+    var viewportWidth = $(window).width();
+    var viewportHeight = $(window).height();
+
+    if (viewportWidth<=770) {
+        $("i.fa").addClass('fa-lg');
+    } else {
+        $("i.fa").addClass('fa-lg');
+    }
+
 }
+
+
+
+$(document).ready(start());
