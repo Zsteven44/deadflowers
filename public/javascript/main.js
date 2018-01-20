95831201e946e4886179cff7210192a1513e1403
Steven Zafrani <zsteven44@gmail.com>
2018-01-20 15:56:49 -0800

    removed unnecessary jqueryimagefit library

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -4,11 +4,12 @@
 function start() {
     var viewportWidth = $(window).width();
     var viewportHeight = $(window).height();
-
-    if (viewportWidth<=770) {
+    console.log("Viewport width: " + viewportWidth);
+    // This will add size-defining class to FA icons based on the user viewports.
+    if (viewportWidth <= 770) {
         $("i.fa").addClass('fa-lg');
     } else {
-        $("i.fa").addClass('fa-lg');
+        $("i.fa").addClass('fa-2x');
     }
 
 }
